966b9930e8a99aa89069249e7654dba7b0e2b6d2
Carlos Davi Dantas <94587505+carlosdavidantas@users.noreply.github.com>
2023-05-30 16:15:03 -0300

    Add files via upload

---
+++ SearchCEP/Adress.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchCEP
+{
+    public class Adress
+    {
+        public string cep { get; set; }
+        public string logradouro { get; set; }
+        public string complemento { get; set; }
+        public string bairro { get; set; }
+        public string localidade { get; set; }
+        public string uf { get; set; }
+        public string ibge { get; set; }
+        public string gia { get; set; }
+        public string ddd { get; set; }
+        public string siafi { get; set; }
+
+        public override string ToString()
+        {
+            return $"CEP: {cep}\nPublicPlace: {logradouro}\nComplement: {complemento}\nNeighborhood: {bairro}\nLocality: {localidade}\nUF: {uf}\nIBGE: {ibge}\nGIA: {gia}\nDDD: {ddd}\nSIAFI: {siafi}";
+        }
+    }
+}
---
+++ SearchCEP/Program.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchCEP
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.Write("CEP: ");
+            string cep = Console.ReadLine();
+            string url = $"http://viacep.com.br/ws/{cep}/json/";
+            Console.WriteLine("=====");
+            using (var client = new HttpClient())
+            {
+                var json = await client.GetStringAsync(url);
+                var adress = JsonConvert.DeserializeObject<Adress>(json);
+                Console.WriteLine(adress);
+                Console.ReadKey();
+            }
+        }
+    }
+}
